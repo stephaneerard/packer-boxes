0792294c438ec976380a8e5a230a6b4aebe8f0f8
Stéphane Erard <stephane.erard@gmail.com>
2020-04-02 00:43:03 +0200

    Making it work with virtualbox nonrotational

--- debian10/scripts/cleanup.sh
+++ debian10/scripts/cleanup.sh
@@ -1,18 +1,14 @@
 #!/bin/bash -eux
 
-# Uninstall Ansible and dependencies.
-pip uninstall ansible
-apt-get remove python-pip python-dev
-
 # Apt cleanup.
-apt autoremove
-apt update
+apt-get autoremove
+apt-get update
 
 # Delete unneeded files.
 rm -f /home/vagrant/*.sh
 
 # Zero out the rest of the free space using dd, then delete the written file.
-dd if=/dev/zero of=/EMPTY bs=1M
+time dd if=/dev/zero of=/EMPTY bs=10M
 rm -f /EMPTY
 
 # Add `sync` so Packer doesn't quit too early, before the large file is deleted.
